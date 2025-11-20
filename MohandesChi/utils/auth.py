935cd2e5ce13adb870edb3e59902ab0c22fcf77f
iimanaghaeii <68458340+iimanaghaeii@users.noreply.github.com>
2025-11-20 11:46:34 +0330

    Delete MohandesChi directory

--- MohandesChi/utils/auth.py
+++
@@ -1,8 +0,0 @@
-from .config import CHANNEL_USERNAME
-
-async def is_member(bot, user_id):
-    try:
-        member = await bot.get_chat_member(CHANNEL_USERNAME, user_id)
-        return member.status in ["member", "administrator", "creator"]
-    except:
-        return False
--- MohandesChi/utils/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
